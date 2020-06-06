cafab24aa9a2f03a8af3fbd94971816563cd43ed
Devin <devin.youngman@move.com>
2020-06-06 15:23:17 -0700

    Giphy SDK Sample

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import GiphySearch from './components/GiphySearch';
+import { Grid } from '@giphy/react-components'
+import { GiphyFetch } from '@giphy/js-fetch-api'
 import request from 'superagent';
 import SearchResults from './components/SearchResults';
 
@@ -8,7 +10,10 @@ class App extends Component {
   constructor() {
     super();
 
+    this.handleSearchChange = this.handleSearchChange.bind(this);
     this.state = {
+      gf: new GiphyFetch('BUt5t6LaXD4aw4Yl2Re0el45WbODWEZA'),
+      fetchGifs: (offset) => this.state.gf.trending({ offset, limit: 10 }),
       results: []
       // results: [
       //     {
@@ -32,8 +37,15 @@ class App extends Component {
     const url = `http://api.giphy.com/v1/gifs/search?q=${userQuery}&api_key=BUt5t6LaXD4aw4Yl2Re0el45WbODWEZA`;
 
     request.get(url, function(err, res) {
-        console.log(res.body.data[0]);
+        //console.log(res.body.data[0]);
     });
+    const gf2 = new GiphyFetch('BUt5t6LaXD4aw4Yl2Re0el45WbODWEZA');
+    this.setState({
+      fetchGifs: (offset) => gf2.search(userQuery, { offset, limit: 10 }),
+    })
+    console.log(this.state.fetchGifs);
+    //this.App.Grid.fetchGifs = this.state.fetchGifs;
+    //this.Grid.fetchGifs = this.state.fetchGifs;
   }
 
   render() {
@@ -44,6 +56,7 @@ class App extends Component {
           <div>
             <GiphySearch onSearchChange={this.handleSearchChange} />
             <SearchResults results={this.state.results} />
+            <Grid width={1000} columns={4} fetchGifs={this.state.fetchGifs} />
           </div>
         </header>
         <footer className="App-header">
