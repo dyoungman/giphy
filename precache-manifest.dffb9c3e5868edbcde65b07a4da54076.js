5075cebc8ef2a0b4b2a4a89522e2fbbf7f8c5ac7
Devin <devin.youngman@move.com>
2020-06-05 17:34:12 -0700

    Updates

--- precache-manifest.dffb9c3e5868edbcde65b07a4da54076.js
+++ precache-manifest.dffb9c3e5868edbcde65b07a4da54076.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "62ec7e2cdacc9076e48f7a4761c710ef",
+    "revision": "df8a874c0dcff465c3af18a3b36cd2f5",
     "url": "/giphy/index.html"
   },
   {
-    "revision": "deda77f10342776d34bf",
-    "url": "/giphy/static/css/main.5f361e03.chunk.css"
+    "revision": "16de2c8cc1fda15c41a2",
+    "url": "/giphy/static/css/main.523f09c5.chunk.css"
   },
   {
     "revision": "e8fcd6b3ca9234ab21cd",
@@ -16,15 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/giphy/static/js/2.2db0a814.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "deda77f10342776d34bf",
-    "url": "/giphy/static/js/main.4ccd88d1.chunk.js"
+    "revision": "16de2c8cc1fda15c41a2",
+    "url": "/giphy/static/js/main.d1fc994d.chunk.js"
   },
   {
     "revision": "e2a49f883b203f181a50",
     "url": "/giphy/static/js/runtime-main.4ecd07b6.js"
   },
   {
-    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/giphy/static/media/logo.5d5d9eef.svg"
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/giphy/static/media/logo.ee7cd8ed.svg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/giphy/precache-manifest.1ca13080cbaa9c50a17a9ecce6f6027e.js"
+  "/giphy/precache-manifest.dffb9c3e5868edbcde65b07a4da54076.js"
 );
 
 self.addEventListener('message', (event) => {
